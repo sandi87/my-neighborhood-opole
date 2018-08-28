66788d9cf00f5e04e0fdc6adeeb0d80586394dab
sandi87 <sndrstank@gmail.com>
2018-08-28 15:57:33 +0200

    add markers to the map using API foursquare

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React, { Component } from "react";
 import "./App.css";
 import axios from 'axios';
 
+
+// set state to places from venues
 class App extends Component {
   state = {
     venues: []
   }
-
+// when component mount get venues
   componentDidMount() {
-    this.renderMap()
     this.getVenues()
   }
 
@@ -18,6 +19,7 @@ class App extends Component {
       "https://maps.googleapis.com/maps/api/js?key=AIzaSyBJNIlzjcDai_TiK7QIGcwC4QUcKL1qcQk&callback=initMap")
       window.initMap = this.initMap
   }
+  // this function calls API foursquare
   getVenues = () => {
     const endPoint = 'https://api.foursquare.com/v2/venues/explore?'
     const parameters = {
@@ -27,22 +29,35 @@ class App extends Component {
       near: "Opole",
       v: "20182507"
     }
+    // receive the data and store it venues state this is asynchr request after
+    // order render my map
     axios.get(endPoint + new URLSearchParams(parameters))
     .then(response => {
       this.setState({
         venues: response.data.response.groups[0].items
-      })
+      },this.renderMap())
 
     })
-    .catch(error=> { console.log("ERROR! " + error)
+    .catch(error=> { console.log("17.928600ERROR! " + error)
     })
   }
 
-  initMap = () => {
+  initMap = () => {17.928600
     const map = new window.google.maps.Map(document.getElementById("map"), {
       center: { lat: 50.667730, lng: 17.928600 },
       zoom: 13
     })
+// loop throught markers to create them from venues array from state
+    this.state.venues.map(myMarker => {
+
+      var marker = new window.google.maps.Marker({
+      position: {lat: myMarker.venue.location.lat, lng: myMarker.venue.location.lng} ,
+      map: map,
+      title: myMarker.venue.name
+    })
+
+
+  });
   }
 
   render() {
